5c86db8162626280d04f1b7b705f8f6f110ab888
bluepython508 <bluepython508@users.noreply.github.com>
2020-11-05 15:43:53 +0000

    Remove commented out old implementation of anvil repair amounts

--- src/main/java/io/github/bluepython508/conveniences/mixin/ItemRepairAmountMixin.java
+++ src/main/java/io/github/bluepython508/conveniences/mixin/ItemRepairAmountMixin.java
@@ -10,15 +10,6 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AnvilScreenHandler.class)
 public class ItemRepairAmountMixin {
-//    @ModifyVariable(method = "updateResult()V", at = @At(value = "INVOKE_ASSIGN", target="Ljava/lang/Math;min(II)I"), name = "o")
-//    public int repairAmount(int old) {
-//        ItemStack toRepair = inventory.getInvStack(0);
-//        ItemStack repairFrom = inventory.getInvStack(1);
-//        if (toRepair.getItem() instanceof AmountRepairable) {
-//            return Math.min(toRepair.getDamage(), ((AmountRepairable) toRepair.getItem()).getRepairAmount(toRepair, repairFrom));
-//        }
-//        return old;
-//    }
     @Redirect(method = "updateResult()V", at = @At(value = "INVOKE", target="Ljava/lang/Math;min(II)I"))
     int repairMin(int a, int b) { // This is almost definitely the wrong way to do this, but I can't find the right one
         Inventory input = ((InputGetter) this).getInput();
